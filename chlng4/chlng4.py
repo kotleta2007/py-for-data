0a6bdfe351f1e51ef5c6bf7662e837823e454f9d
Mark Tropin <kotleta2007@protonmail.ch>
2019-02-09 21:13:25 +0300

    Challenge 4 done

--- chlng4/chlng4.py
+++ chlng4/chlng4.py
@@ -16,6 +16,9 @@ plt.switch_backend('Qt5Agg')
 dates = []
 prices = []
 
+# λ function for converting arrays to NumPy matrices
+to_np = lambda n : np.array(n).reshape(1,-1)
+
 # Writing CSV data to lists
 def get_data(filename):
     with open(filename, 'r') as csvfile:
@@ -34,7 +37,7 @@ def predict_prices(dates, prices, x):
     # Creating 3 SVR models
     svr_lin = SVR(kernel='linear', C=1e3)
     svr_poly = SVR(kernel='poly', C=1e3, degree=2)
-    svr_rbf = SVR(kernel='rbf', C=1e3, gamma='auto')
+    svr_rbf = SVR(kernel='rbf', C=1e3, gamma='scale')
     
     # Training them
     svr_lin.fit(dates, prices)
@@ -55,6 +58,44 @@ def predict_prices(dates, prices, x):
     # Returning the predicted price for given day
     return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
     
+def predict_sentiment(company_name):
+    # define topic of research
+    topic = company_name
+    
+    # Lists to be filled with Twitter data
+    polarity = []
+    subjectivity = []
+
+    # Keys for the API
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
+
+    # Connecting to the API + authorization
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    # Searching for tweets...
+    public_tweets = api.search(topic)
+    
+    # For all tweets found, analyze them and save their polarity and subjectivity
+    for tweet in public_tweets:
+        tweet_text = tweet.text
+        analysis = TextBlob(tweet_text).sentiment
+        polarity.append(analysis.polarity)
+        subjectivity.append(analysis.subjectivity)
+    
+    plt.figure()
+    plt.scatter(polarity, subjectivity, color='black', label=company_name)
+    plt.xlabel('Polarity')
+    plt.ylabel('Subjectivity')
+    plt.title('Sentiment analysis')
+    plt.legend()
+    plt.show()
+    return
     
 get_data("GOOG.csv")
-print(predict_prices(dates, prices, np.array(35).reshape(1,-1)))
+print(predict_prices(dates, prices, to_np(35)))
+predict_sentiment("Google")
