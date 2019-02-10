7832aab5ccb23e0988f436df56e4552104660166
Mark Tropin <kotleta2007@protonmail.ch>
2019-02-10 23:26:34 +0300

    Fixed bugs

--- chlng5/deep_dream.py
+++ chlng5/deep_dream.py
@@ -2,15 +2,15 @@ import numpy as np
 from functools import partial
 import PIL.Image
 import tensorflow as tf
+import matplotlib.pyplot as plt
 import urllib.request
 import os
 import zipfile
-import matplotlib.pyplot as plt
 
 def main():
-    # Step 1 - download Google's pre-trained neural network
+    #Step 1 - download google's pre-trained neural network
     url = 'https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip'
-    data_dir = "./data"
+    data_dir = './data/'
     model_name = os.path.split(url)[-1]
     local_zip_file = os.path.join(data_dir, model_name)
     if not os.path.exists(local_zip_file):
@@ -21,29 +21,34 @@ def main():
         # Extract
         with zipfile.ZipFile(local_zip_file, 'r') as zip_ref:
             zip_ref.extractall(data_dir)
-    
+  
     # start with a gray image with a little noise
     img_noise = np.random.uniform(size=(224,224,3)) + 100.0
-    
+  
     model_fn = 'tensorflow_inception_graph.pb'
     
-    # Step 2 - Creating TensorFlow session and loading the model
+    #Step 2 - Creating Tensorflow session and loading the model
     graph = tf.Graph()
     sess = tf.InteractiveSession(graph=graph)
     with tf.gfile.FastGFile(os.path.join(data_dir, model_fn), 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
-    t_input = tf.placeholder(np.float32, name='input') # define input tensor
+    t_input = tf.placeholder(np.float32, name='input') # define the input tensor
     imagenet_mean = 117.0
     t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
-    tf.import_graph_def(graph_def, {'input': t_preprocessed})
+    tf.import_graph_def(graph_def, {'input':t_preprocessed})
     
     layers = [op.name for op in graph.get_operations() if op.type=='Conv2D' and 'import/' in op.name]
     feature_nums = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
     
-    print("Number of layers: ", len(layers))
-    print("Total number of feature channels: ", sum(feature_nums))
-    
+    print('Number of layers', len(layers))
+    print('Total number of feature channels:', sum(feature_nums))
+  
+ #####HELPER FUNCTIONS. I didn't go over these in the video for times sake. They are mostly just formatting functions. Scroll 
+ #to the bottom #########################################################################################################
+ ########################################################################################################################
+ ############################################################
+ 
     # Helper functions for TF Graph visualization
     #pylint: disable=unused-variable
     def strip_consts(graph_def, max_const_size=32):
@@ -125,42 +130,57 @@ def main():
                 sub = img_shift[y:y+sz,x:x+sz]
                 g = sess.run(t_grad, {t_input:sub})
                 grad[y:y+sz,x:x+sz] = g
-        return np.roll(np.roll(grad, -sx, 1), -sy, 0) 
+        return np.roll(np.roll(grad, -sx, 1), -sy, 0)    
+
+    #BACK TO CODE IN THE VIDEO###########################################################################################
+    ########################################################################################################
+    ##############################################################################
     
-    def render_deepdream(t_obj, img0=img_noise, iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
-        t_score = tf.reduce_mean(t_obj) # defining optimization objective
-        t_grad = tf.gradients(t_score, t_input)[0]
+    #CHALLENGE - Write a function that outputs a deep dream video
+    #def render_deepdreamvideo():
         
-        # split the image into octaves
+        
+    def render_deepdream(t_obj, img0=img_noise,
+                         iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
+        t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+        t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+    
+        # split the image into a number of octaves
         img = img0
         octaves = []
         for _ in range(octave_n-1):
             hw = img.shape[:2]
-            lo = resize(img, np.int32(np.float32(hw) / octave_scale))
-            hi = img - resize(lo, hw)
+            lo = resize(img, np.int32(np.float32(hw)/octave_scale))
+            hi = img-resize(lo, hw)
             img = lo
             octaves.append(hi)
         
         # generate details octave by octave
         for octave in range(octave_n):
-            if octave > 0:
+            if octave>0:
                 hi = octaves[-octave]
-                img = resize(img, hi.shape[:2]) + hi
+                img = resize(img, hi.shape[:2])+hi
             for _ in range(iter_n):
                 g = calc_grad_tiled(img, t_grad)
-                img += g * (step / (np.abs(g).mean()+1e7))
-            # Step 5 - output deep dreamed image
+                img += g*(step / (np.abs(g).mean()+1e-7))
+            
+            #this will usually be like 3 or 4 octaves
+            #Step 5 output deep dream image via matplotlib
             showarray(img/255.0)
-    
-    # Step 3 - Pick a layer to enhance our image
+            
+         
+  
+   	#Step 3 - Pick a layer to enhance our image
     layer = 'mixed4d_3x3_bottleneck_pre_relu'
-    channel = 139
+    channel = 139 # picking some feature channel to visualize
     
-    img0 = PIL.Image.open('gogol.jpg')
+    #open image
+    img0 = PIL.Image.open('mon.jpg')
     img0 = np.float32(img0)
-    
+     
     #Step 4 - Apply gradient ascent to that layer
     render_deepdream(tf.square(T('mixed4c')), img0)
-    
+      
+  
 if __name__ == '__main__':
     main()
